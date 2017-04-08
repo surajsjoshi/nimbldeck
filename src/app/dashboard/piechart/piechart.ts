31501c6f72a9d21fff7fbddae29f889f577cc723
saswat.sethy <saswat.sethy@germinait.com>
2017-04-08 13:10:17 +0530

    pie chart numbers

--- src/app/dashboard/piechart/piechart.ts
+++ src/app/dashboard/piechart/piechart.ts
@@ -30,8 +30,39 @@ export class PieChartComponent implements OnInit {
           return dataLabel + value;
         }
       }
- }
+ },
 
+   animation: {
+        onComplete: function() {
+           this.chart.controller.draw();
+           let ctx = this.chart.ctx;
+           ctx.textAlign = 'center';
+           ctx.textBaseline = 'bottom';
+           this.data.datasets.forEach(function (dataset) {
+            for (let i = 0; i < dataset.data.length; i++) {
+              let model = dataset._meta[Object.keys(dataset._meta)[0]].data[i]._model,
+              total = dataset._meta[Object.keys(dataset._meta)[0]].total,
+              mid_radius = model.innerRadius + (model.outerRadius - model.innerRadius) / 2,
+              start_angle = model.startAngle,
+              end_angle = model.endAngle,
+              mid_angle = start_angle + (end_angle - start_angle)/2;
+
+              let x = mid_radius * Math.cos(mid_angle);
+              let y = mid_radius * Math.sin(mid_angle);
+
+              ctx.fillStyle = '#fff';
+              if (i === 3)  { // Darker text color for lighter background
+                ctx.fillStyle = '#444';
+              }
+            let percent = String(Math.round(dataset.data[i] / total* 100)) + ' % ';
+            ctx.fillText(dataset.data[i], model.x + x, model.y + y);
+          // Display percent in another line, line break doesn't work for fillText
+            ctx.fillText(percent, model.x + x, model.y + y + 15);
+        }
+      });
+
+        }
+   }
 
  };
 
@@ -51,3 +82,4 @@ export class PieChartComponent implements OnInit {
 
   // Pie
 }
+
