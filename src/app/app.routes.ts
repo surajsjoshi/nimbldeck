766f049bdf69f8a073f2a2a4526788a747e75f03
saswat.sethy <saswat.sethy@germinait.com>
2017-05-26 11:31:02 +0530

    url pattern changed to hash

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -31,4 +31,4 @@ const routes: Routes = [
   }
 ];
 
-export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
+export const routing: ModuleWithProviders = RouterModule.forRoot(routes, {useHash: true});
