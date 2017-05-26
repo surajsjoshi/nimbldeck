c4fe414fe080b31fbc5e1f3a7a8e32f72faabc5d
Saswat Sethy <sksethy@Saswats-MacBook-Air.local>
2017-05-26 20:05:18 +0530

    session export ui updated

--- src/app/dashboard/tabs/tabscomponent.ts
+++ src/app/dashboard/tabs/tabscomponent.ts
@@ -1,7 +1,12 @@
-
-import { ConfigurationService } from '../../services/configuration.service';
 import { TabComponent } from './tabcomponent';
-import { Component, AfterContentInit, QueryList, ContentChildren } from '@angular/core';
+import { Session } from '../../shared/models/session';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SessionService } from '../../services/session.service';
+import { ConfigurationService } from '../../services/configuration.service';
+import { Component, AfterContentInit, QueryList, ContentChildren,  OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
 declare var ga: any;
 declare var mixpanel: any;
 
@@ -10,11 +15,35 @@ declare var mixpanel: any;
   templateUrl: './tabs.html',
   styleUrls: ['./tabs.css']
 })
-export class TabsComponent implements AfterContentInit {
+export class TabsComponent implements AfterContentInit , OnInit , OnDestroy {
+
+  session: Session;
+  private subscription: Subscription;
 
   @ContentChildren(TabComponent) tabs: QueryList<TabComponent>;
 
-  constructor(private conf: ConfigurationService) {}
+  constructor(private conf: ConfigurationService, 
+      private toastr: ToastsManager,
+      private route: ActivatedRoute,
+      private sessionService: SessionService) {
+
+  }
+
+  ngOnInit() {
+    this.subscription = this.route.params.subscribe(params => {
+         this.sessionService.getSession(params['id'])
+        .subscribe(sess => this.mapSession(sess),
+            (error => console.log(error)));
+        });
+ }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  private mapSession(response) {
+    this.session = response;
+  }
 
   // contentChildren are set
   ngAfterContentInit() {
@@ -36,6 +65,16 @@ export class TabsComponent implements AfterContentInit {
     mixpanel.track(selectedTab.title, {'user': this.conf.getUser().emailId});
     ga('set', 'userId', this.conf.getUser().userId);
     ga('send', 'pageview', '/sessions/dashboard/' + selectedTab.title);
+  }
+
+   sessionExport(event) {
+    this.sessionService.exportSession(this.session.session_id).subscribe(
+      (resp => this.exportSuccess(resp)),
+        (error => console.log(error)));
+  }
+
+  exportSuccess(response: any){
+       this.toastr.success('You will soon receive the export at your registered emailid', 'Success!');
   }
 
 }
